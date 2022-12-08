db4291d45ca76b1a472846a2928520752d675f14
Trevor Ramdhan <trevor.ramdhan@rci.rogers.com>
2022-12-08 07:53:06 -0500

    add mongoose connection

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 4000;
 
+// mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/productsdb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
-app.get('/', (req, res) =>
-    res.send(`Store server running on port ${PORT}`)
-);
+app.get("/", (req, res) => res.send(`Store server running on port ${PORT}`));
 
-app.listen(PORT, () => 
-    console.log(`Your server is running on port ${PORT}`)
-);
+app.listen(PORT, () => console.log(`Your server is running on port ${PORT}`));
